9b53db32fe4fde6454e1e604c54829b2f329f7b0
Ileana <ferchu91garcia@gmail.com>
2024-07-02 21:24:19 -0300

    eliminando paciente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,11 +1,13 @@
 import { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
+import { usePatientStore } from "../store";
 
 type PatientDetailsProps = {
   patient: Patient;
 };
 
 function PatientDetails({ patient }: PatientDetailsProps) {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
   return (
     <div className=" mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -14,6 +16,21 @@ function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Email" data={patient.email} />
       <PatientDetailItem label="Fecha de Alta" data={patient.date.toString()} />
       <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+      <div className=" flex justify-between gap-3 mt-10">
+        <button
+          type="button"
+          className=" py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          onClick={() => deletePatient(patient.id)}
+          type="button"
+          className=" py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 }
