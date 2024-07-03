f8c3885a21ec765d74a473015c6476f44d971469
Ileana <ferchu91garcia@gmail.com>
2024-07-03 14:20:57 -0300

    capturando por id el paciente a modificar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -8,6 +8,7 @@ type PatientDetailsProps = {
 
 function PatientDetails({ patient }: PatientDetailsProps) {
   const deletePatient = usePatientStore((state) => state.deletePatient);
+  const getPatientById = usePatientStore((state) => state.getPatientById);
   return (
     <div className=" mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -18,6 +19,7 @@ function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Sintomas" data={patient.symptoms} />
       <div className=" flex justify-between gap-3 mt-10">
         <button
+          onClick={() => getPatientById(patient.id)}
           type="button"
           className=" py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
         >
